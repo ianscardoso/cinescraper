ba3d02a5a093bb6cac86b4b933b107bcbdc60466
Ian.Cardoso <Ian.Cardoso@targetsistemas.com.br>
2019-04-15 14:07:35 -0300

    added nodemailer module and .gitignore

--- index.js
+++ index.js
@@ -1,36 +1,80 @@
-//const axios = require('axios');
-const Nightmare = require('nightmare')
+// node_modules
+const Nightmare = require('nightmare');
 const cheerio = require('cheerio');
-const nightmare = Nightmare({ show: false })
+const nodemailer = require('nodemailer');
+const config = require('./config.js');
+
+const nightmare = Nightmare({ show: false });
 const url = 'https://www.cinepolis.com.br/programacao/ribeirao+preto/30.html';
 let $;
 let movies = [];
 
-nightmare
-    .goto(url)
-    .wait('#programacao_cinepolis')
-    .evaluate(() => document.querySelector('#programacao_cinepolis').innerHTML)
-    .end()
-    .then(response => {
-        $ = cheerio.load(response);
-        $('.tituloPelicula').each((i, movie) => {
-            let sessions = [];
-            $(movie).find($('time', '.horarioExp')).each((i, time) => {
-                sessions.push($(time).attr('alt'));
+function scrap() {
+    nightmare
+        .goto(url)
+        .wait('#programacao_cinepolis')
+        .evaluate(() => document.querySelector('#programacao_cinepolis').innerHTML)
+        .end()
+        .then(response => {
+            $ = cheerio.load(response);
+            $('.tituloPelicula').each((i, movie) => {
+                let sessions = [];
+                $(movie).find($('time', '.horarioExp')).each((i, time) => {
+                    sessions.push($(time).attr('alt'));
+                })
+
+                movies.push({
+                    title: $(movie).find('.datalayer-movie').text(),
+                    duration: $(movie).find('.duracion').text(),
+                    sessions: sessions
+                })
             })
 
-            movies.push({
-                title: $(movie).find('.datalayer-movie').text(),
-                duration: $(movie).find('.duracion').text(),
-                sessions: sessions
-            })
-        })
+            text(movies);
 
-        movies.forEach(movie => {
-            console.log(movie);
+            return true;
         })
-    })
-    .catch(error => {
-        console.log(error);
+        .catch(error => {
+            console.log(error);
+
+            return false;
+        });
+}
+
+function sendMail(text) {
+    let transporter = nodemailer.createTransport({
+        service: 'outlook',
+        auth: {
+            user: `${config.emailUser}`,
+            pass: `${config.emailPassword}` 
+        }
     });
 
+    let mailOptions = {
+        from: `${config.emailUser}`,
+        to: 'ianscardoso@hotmail.com',
+        subject: 'Filmes de hoje',
+        text: `${text}`
+    }
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error)
+            console.log(error);
+        else
+            console.log('Email sent: ' + info.response);
+    })
+}
+
+function text(movieList) {
+    let text = 'Filmes no Cinépolis:\n\n';
+
+    movieList.forEach(movie => {
+        text += `\nTítulo: ${movie.title}
+        Duração: ${movie.duration}
+        Horários: ${movie.sessions}\n`
+    });
+
+    sendMail(text);
+}
+
+scrap();
